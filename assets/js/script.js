27cc805fb82145c4ffaef6c4e5214e5e2ae8d696
Your Name <sumitkhan.dhana@gmail.com>
2023-08-21 14:32:41 +0530

    add onclick

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,20 @@
              myNav.classList.toggle("show");
         }
 
-        
-        function changeTab(tabIndex) {
-          const tabs = document.querySelectorAll('.tab');
-          const tabContents = document.querySelectorAll('.tab-content');
-        
-          tabs.forEach((tab, index) => {
-            if (index === tabIndex) {
-              tab.classList.add('active');
-              tabContents[index].classList.add('active');
-            } else {
-              tab.classList.remove('active');
-              tabContents[index].classList.remove('active');
-            }
-          });
+
+        function openCity(evt, cityName) {
+          var i, tabcontent, tablinks;
+          tabcontent = document.getElementsByClassName("tabcontent");
+          for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+          }
+          tablinks = document.getElementsByClassName("tablinks");
+          for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+          }
+          document.getElementById(cityName).style.display = "block";
+          evt.currentTarget.className += " active";
         }
+        
+        // Get the element with id="defaultOpen" and click on it
+        document.getElementById("defaultOpen").click();
